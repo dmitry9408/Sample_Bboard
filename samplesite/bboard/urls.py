c0a8484c03b3a760182b4de2457f14ceea0502b3
Your Name <dmitrii.kozlov.1994@mail.rum>
2023-12-14 13:01:38 +0300

    Первая версия приложения Bboard

---
+++ samplesite/bboard/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import index, rubric_bbs
+
+urlpatterns = [
+    path('<int:rubric_id>/', rubric_bbs, name='rubric_bbs'),
+    path('', index, name='index'),
+]
---
+++ samplesite/bboard/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from .models import Bb, Rubric
+
+# Create your views here.
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'index.html', context)
+
+
+def rubric_bbs(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'rubric_bbs.html', context)
